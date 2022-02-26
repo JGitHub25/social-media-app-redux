b10a78ae456834842ebddd5d3bfff6d2b25882cf
jpeg25 <jairo25509@hotmail.com>
2022-02-26 15:37:28 -0500

    posts include user name.

--- src/features/posts/AddPostForm.js
+++ src/features/posts/AddPostForm.js
@@ -1,26 +1,37 @@
 import React, { useState } from 'react'
-
-import { useDispatch } from 'react-redux'
+import { useDispatch, useSelector } from 'react-redux'
 
 import { addPost } from './postsSlice'
 
 export const AddPostForm = () => {
   const [title, setTitle] = useState('')
   const [content, setContent] = useState('')
+  const [userId, setUserId] = useState('')
 
   const dispatch = useDispatch()
 
+  const users = useSelector((state) => state.users)
+
   const handleTitleChange = (e) => setTitle(e.target.value)
   const handlePostChange = (e) => setContent(e.target.value)
+  const handleUserChange = (e) => setUserId(e.target.value)
 
   const savePost = (e) => {
     e.preventDefault()
     if (title && content) {
-      dispatch(addPost(title, content))
+      dispatch(addPost(title, content, userId))
       setTitle('')
       setContent('')
     }
   }
+
+  const canSave = Boolean(title) && Boolean(content) && Boolean(userId)
+
+  const usersOptions = users.map((user) => (
+    <option key={user.id} value={user.id}>
+      {user.name}
+    </option>
+  ))
 
   return (
     <section>
@@ -34,6 +45,11 @@ export const AddPostForm = () => {
           value={title}
           onChange={handleTitleChange}
         />
+        <label htmlFor="postAuthor">Author:</label>
+        <select id="postAuthor" value={userId} onChange={handleUserChange}>
+          <option value=""></option>
+          {usersOptions}
+        </select>
         <label htmlFor="postContent">Content:</label>
         <textarea
           id="postContent"
@@ -41,7 +57,9 @@ export const AddPostForm = () => {
           value={content}
           onChange={handlePostChange}
         />
-        <button type="submit">Save Post</button>
+        <button type="submit" disabled={!canSave}>
+          Save Post
+        </button>
       </form>
     </section>
   )
